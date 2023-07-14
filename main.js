5f46f76187d1242ba78a97fb76d86c35fdb15158
itsdrews <134967463+itsdrews@users.noreply.github.com>
2023-07-14 18:08:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+alert ("Hello World!");
+let a = Number(5);
+let b = Number(23123);
+alert (a+b);
+let test1 = 23;
+if (typeof test1== typeof '34') {
+ alert( "Não é um número");
+ 
+  
+} else {
+  alert( "É um número");
+  
+}
+let test2 = 43;
+if (typeof test2 == typeof "") {
+  alert("É uma STRING");
+  
+} else {
+  alert("Não é uma string!!")
+  
+}
+let test3 = false;
+if (typeof test3 == typeof true){
+  alert("É Boolean!");
+  
+}else{
+  alert("Não é boolean");
+
+}
+alert(b-a);
+alert(b*a);
+alert(b/a);
+let test4 = 12386713;
+if (test4 % 2 == 0) {
+  alert("É par!");
+  
+} else {
+  alert("Não é par!");
+
+  
+}
+let test5 = 123834176;
+if (test5 % 2 != 0) {
+  alert("É impar!");
+  
+} else {
+  alert("Não é impar!");
+
+  
+}
+
+
+
